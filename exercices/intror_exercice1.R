cb4a28152f5b3ab23ad1a02872a96917c0bcc04f
Marie Etienne <marie-pierre.etienne@agrocampus-ouest.fr>
2018-10-15 16:54:28 +0200

    website, version 0.1

---
+++ exercices/intror_exercice1.R
@@ -0,0 +1,18 @@
+##########################################################################
+##### Ceci est une ligne de commentaires 
+##### parce que les commentaires c'est utile pour dire ce qu'on fait
+##### par exemple 
+#####
+#####  Script d'illustration pour la prise en main de RStudio
+###########################################################################
+
+## charger un jeu de données déjà disponible dans R
+data(iris)
+
+## illustrer l'aspect objet
+class(iris)
+
+
+## completion : taper class(iris$S) puis la touche TAB
+class(iris$Sepal.Length)
+class(iris$Species)
